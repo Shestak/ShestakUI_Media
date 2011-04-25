b2ce9803786c55002078859cdf1ab2aa53f97e39
Shestak <misha.shestakov@gmail.com>
2011-04-25 18:32:56 +0400

    Added same textures.

--- ShestakUI_Media.lua
+++ ShestakUI_Media.lua
@@ -19,6 +19,7 @@ end
 LSM:Register("border", "Normal", "Interface\\AddOns\\ShestakUI_Media\\Media\\Borders\\Normal.tga")
 LSM:Register("border", "Shadow", "Interface\\AddOns\\ShestakUI_Media\\Media\\Borders\\Shadow.tga")
 LSM:Register("border", "Slim", "Interface\\AddOns\\ShestakUI_Media\\Media\\Borders\\Slim.tga")
+LSM:Register("border", "Thin", "Interface\\AddOns\\ShestakUI_Media\\Media\\Borders\\Thin.tga")
 
 -- Fonts
 LSM:Register("font", "Calibri", "Interface\\AddOns\\ShestakUI_Media\\Media\\Fonts\\Calibri.ttf", LOCALE_MASK)
@@ -35,6 +36,7 @@ LSM:Register("sound", "Whisper", "Interface\\AddOns\\ShestakUI_Media\\Media\\Sou
 -- Statusbars
 LSM:Register("statusbar", "BantoBar", "Interface\\AddOns\\ShestakUI_Media\\Media\\Statusbars\\BantoBar.tga")
 LSM:Register("statusbar", "Bars", "Interface\\AddOns\\ShestakUI_Media\\Media\\Statusbars\\Bars.tga")
+LSM:Register("statusbar", "Bright", "Interface\\AddOns\\ShestakUI_Media\\Media\\Statusbars\\Bright.tga")
 LSM:Register("statusbar", "DarkBottom", "Interface\\AddOns\\ShestakUI_Media\\Media\\Statusbars\\DarkBottom.tga")
 LSM:Register("statusbar", "Empty", "Interface\\AddOns\\ShestakUI_Media\\Media\\Statusbars\\Empty.tga")
 LSM:Register("statusbar", "Flat", "Interface\\AddOns\\ShestakUI_Media\\Media\\Statusbars\\Flat.tga")
